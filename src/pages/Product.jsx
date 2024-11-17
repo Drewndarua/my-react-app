08d987330b8c0bdd2ea5c54205b56e365d8f2421
Andrew Kinyua <andrewkinyua500@gmail.com>
2024-11-17 23:54:01 +0300

    updated products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 export const Product = () => {
-  return <div></div>;
+  const { productId } = useParams();
+  const { products } = useContext(ShopContext);
+  const { productData, seProductData } = useState(false);
+  const [image, setImage] = useState("");
+
+  const fetchProductData = useCallback(() => {
+    const item = products.find((item) => item._id === productId);
+    if (item) {
+      seProductData(item);
+      console.log(item);
+      setImage(item.image[0]);
+    }
+  }, [productId, products]);
+
+  useEffect(() => {
+    fetchProductData();
+  }, [fetchProductData]);
+
+  return productData ? (
+    <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
+      <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
+        {/* Product Images */}
+        <div className="flex-1 flex-col-reverse gap-3 sm:flex-row">
+          <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-normal sm:w-[18.7%] w-full">
+            {productData.image.map((item, index) => (
+              <img
+                src={item}
+                key={index}
+                className="w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer"
+                alt={`Product ${index}`}
+              />
+            ))}
+          </div>
+          <div className="w-full sm:w-[80%]">
+            <img className="w-full h-auto" src={image} alt="Main Product" />
+          </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="opacity-0"></div>
+  );
 };
 
 export default Product;
